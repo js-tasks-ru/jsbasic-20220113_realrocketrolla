4c25e078d87c5d645917936844ae7e9fbd5e6218
realrocketrolla <realrocketrolla@yandex.ru>
2022-01-29 21:39:33 +0300

    решение 4 и 5 задачи

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
-function getMinMax(str) {
-  // ваш код...
+"use strict";
+function getMinMax(str) { // Реализуйте функцию, которая принимает строку с числами и возвращает объект, содержащий минимальное и максимальное значения чисел из этой строки.
+  let returnValue = str
+   .split(" ")
+   .filter((i) => isFinite(i))
+   .map((i) => +i);
+
+  return {
+    min: Math.min(...returnValue),
+    max: Math.max(...returnValue)
+  };
 }
+
