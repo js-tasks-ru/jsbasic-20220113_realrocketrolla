504f63d3bd69b5e7e752186ced61a41aba5eab9e
realrocketrolla <realrocketrolla@yandex.ru>
2022-02-03 12:20:44 +0300

    решение задач 4го модуля

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,19 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let curRowNumber = 0;
+  let curColNumber = 0;
+  
+  for (let row of table.rows) { 
+    curColNumber = 0;
+  
+    for (let cell of row.cells) { 
+      if (curRowNumber === curColNumber) { 
+        cell.style.backgroundColor = 'red';
+      }
+  
+      curColNumber++;
+    }
+  
+    curRowNumber++;
+  }
 }
+
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  let headers = [];
+  for (let header of table.tHead.rows[0].cells) { 
+    headers.push(header.innerText);
+  }
+
+  for (let row of table.tBodies[0].rows) {
+    let availability = row.cells[headers.indexOf('Status')].dataset.available;
+    if (availability === undefined) {
+      row.setAttribute('hidden', true);
+    } else if (availability === 'true') { 
+      row.classList.add('available');
+    } else if (availability === 'false') {
+      row.classList.add('unavailable');
+    }
+
+    let gender = row.cells[headers.indexOf('Gender')].innerText;
+    if (gender === 'm') { 
+      row.classList.add('male');
+    } else if (gender === 'f') {
+      row.classList.add('female');
+    } 
+
+    let age = Number(row.cells[headers.indexOf('Age')].innerText);
+
+    if (age < 18) { 
+      row.style['text-decoration'] = 'line-through';
+    }
+  }
 }
