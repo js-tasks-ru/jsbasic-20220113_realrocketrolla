339f28324ffe54b2289421350b0f56884a2d7e10
realrocketrolla <realrocketrolla@yandex.ru>
2022-02-26 14:10:45 +0300

    8,9

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -1,34 +1,49 @@
 export default class Cart {
-  cartItems = []; // [product: {...}, count: N]
+  cartItems = []; 
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product)
+      return;
+    let item = this.cartItems.find(item => item.product === product);
+
+    if (item) {
+      item.count += 1;
+    } else {
+      item = {
+        product,
+        count: 1
+      };
+      this.cartItems.push(item);
+    }
+    this.onProductUpdate(item);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let item = this.cartItems.find(el => el.product.id === productId);
+    item.count += amount;
+    if (!item.count) {
+      this.cartItems.splice(this.cartItems.indexOf(item),1);
+    }
+    this.onProductUpdate(item);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((totalCount, item) => totalCount + item.count, 0);
+  }
+  getTotalPrice() {
+    return this.cartItems.reduce((totalPrice, item) => totalPrice + (item.product.price * item.count), 0);
   }
 
-  getTotalPrice() {
-    // ваш код
-  }
 
   onProductUpdate(cartItem) {
-    // реализуем в следующей задаче
-
     this.cartIcon.update(this);
   }
 }
-
